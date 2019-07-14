81fe8d3a11a7a649c8db57a3647c949dbe601f87
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2019-07-14 19:06:35 -0300

    - Move the zipcodesearch to the folder services - Fix the references in import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ZipCodeSearchService } from './zipcodesearch.service';
+import { ZipCodeSearchService } from 'src/app/services/zipcodesearch.service';
 
 @NgModule({
   declarations: [
